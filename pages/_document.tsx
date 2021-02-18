05c849b15cc82e4f92deef2a74f695ec2aa06f63
Vince <vincent@bavitz.org>
2021-02-18 14:52:11 +1100

    Ready for build

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -27,6 +27,7 @@ export default class CustomDocument extends Document<any> {
           <link rel="manifest" href="/site.webmanifest" />
           <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
           <meta name="theme-color" content="#ffffff" />
+
           {this.props?.styleTags}
         </Head>
         <body>

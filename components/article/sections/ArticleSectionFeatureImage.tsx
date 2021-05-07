2efcc00d529bdc8384bd5a12210e5e6ce141971c
Lucas Phang <lucasphang98@gmail.com>
2021-05-07 15:17:41 +1000

    Add alt text to feature image on blog post

--- components/article/sections/ArticleSectionFeatureImage.tsx
+++ components/article/sections/ArticleSectionFeatureImage.tsx
@@ -2,15 +2,16 @@ import { IFigureImage } from '../../../types/cms';
 
 interface Props {
   featureImage: IFigureImage;
+  title: string;
 }
 
-export function ArticleSectionFeatureImage({ featureImage }: Props) {
+export function ArticleSectionFeatureImage({ featureImage, title }: Props) {
   return (
     <div className="w-full pb-4 desktop:pb-0">
       <div className="relative w-full mb-4 aspect-w-14 aspect-h-8">
         <img
           src={featureImage?.imageUrl}
-          alt={featureImage?.description ?? ''}
+          alt={featureImage?.description ?? title}
           className="object-cover rounded"
         />
       </div>

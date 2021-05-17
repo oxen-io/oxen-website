4f94bb3b6387246744c5ab5c79d15685d0da8de9
Lucas Phang <46293489+lucasPDY@users.noreply.github.com>
2021-05-17 05:29:37 +0000

    Make FAQ Styling Consistent & Image Update

--- pages/faq.tsx
+++ pages/faq.tsx
@@ -6,10 +6,8 @@ import { SideMenuItem } from '../state/navigation';
 import { generateTitle, generateURL } from '../utils/metadata';
 import { CmsApi } from '../services/cms';
 import { IFAQItem } from '../types/cms';
-import { RichBody } from '../components/RichBody';
-import { Contained } from '../components/Contained';
 import { Accordion } from '../components/Accordion';
-import classNames from 'classnames';
+import { Contained } from '../components/Contained';
 
 export const getServerSideProps: GetServerSideProps = async context => {
   const cms = new CmsApi();
@@ -32,7 +30,7 @@ interface Props {
   total: number;
 }
 function FAQ(props: Props) {
-  const { faqItems, total } = props;
+  const { faqItems } = props;
 
   const pageTitle = generateTitle(
     NAVIGATION.SIDE_MENU_ITEMS[SideMenuItem.FAQ].label,
@@ -40,7 +38,8 @@ function FAQ(props: Props) {
   const pageURL = generateURL(
     NAVIGATION.SIDE_MENU_ITEMS[SideMenuItem.FAQ].href,
   );
-  const imageURL = `${METADATA.OXEN_HOST_URL}/hero.svg`;
+  const imagePathLocal = 'img/what_is_oxen.png';
+  const imageURL = `${METADATA.OXEN_HOST_URL}/${imagePathLocal}`;
 
   return (
     <>
@@ -63,26 +62,33 @@ function FAQ(props: Props) {
         <meta name="twitter:image" content={imageURL} />
       </Head>
 
-      <div>
-        <div className="flex items-center justify-center">
-          <img src={`hero.svg`} className="w-full" />
+      <div className="bg-alt">
+        <div className="relative flex items-center justify-center w-full h-full pt-3 bg-gradient-to-bl from-hyper to-blush">
+          <img
+            style={{ maxHeight: '33vh' }}
+            src={imagePathLocal}
+            className="object-contain w-full"
+          />
         </div>
-      </div>
 
-      <div className="w-full h-full">
-        <div className="flex flex-col pb-6 mx-4">
-          <div>
-            <h1 className="mt-6 mb-3 text-3xl font-medium text-left tablet:text-4xl font-prompt text-primary">
-              FAQ
-            </h1>
-          </div>
-
-          {faqItems.map(faqItem => (
-            <div key={faqItem.id}>
-              <Accordion question={faqItem.question} answer={faqItem.answer} />
+        <Contained>
+          <div className="flex flex-col pb-6 mx-4">
+            <div>
+              <h1 className="mt-6 mb-3 text-3xl font-medium text-left tablet:text-4xl font-prompt text-primary">
+                FAQ
+              </h1>
             </div>
-          ))}
-        </div>
+
+            {faqItems.map(faqItem => (
+              <div key={faqItem.id}>
+                <Accordion
+                  question={faqItem.question}
+                  answer={faqItem.answer}
+                />
+              </div>
+            ))}
+          </div>
+        </Contained>
       </div>
     </>
   );

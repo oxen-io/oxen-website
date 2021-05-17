8df5830723cc936dc2b7010760fef8f93d4f76d1
Lucas Phang <46293489+lucasPDY@users.noreply.github.com>
2021-05-17 04:36:27 +0000

    Add redirect to  Monetisation Blog Post

--- next.config.js
+++ next.config.js
@@ -22,6 +22,16 @@ const nextConfig = {
     CONTENTFUL_SPACE_ID: process.env.CONTENTFUL_SPACE_ID,
     CONTENTFUL_ACCESS_TOKEN: process.env.CONTENTFUL_ACCESS_TOKEN,
   },
+  async redirects() {
+    return [
+      {
+        source: '/blog/session-the-road-to-monetisation-and-oxen-value-capture',
+        destination: '/blog/session-the-road-to-monetisation',
+        permanent: true,
+      },
+    ];
+  },
 };
 
 module.exports = withPlugins([withFonts, withSvgr], nextConfig);
+ 

7bb4c720c69453ee469414867fa1494ffb0abfbc
Lucas Phang <lucasphang98@gmail.com>
2021-06-29 18:35:37 +1000

    Adjust blog image padding

--- components/article/sections/ArticleSectionFeatureImage.tsx
+++ components/article/sections/ArticleSectionFeatureImage.tsx
@@ -1,4 +1,6 @@
 import { IFigureImage } from '../../../types/cms';
+import { Contained } from '../../Contained';
+import { ArticleContained } from '../../ArticleContained';
 
 interface Props {
   featureImage: IFigureImage;
@@ -8,18 +10,23 @@ interface Props {
 export function ArticleSectionFeatureImage({ featureImage, title }: Props) {
   return (
     <div className="w-full pb-4 desktop:pb-0">
-      <div className="relative w-full max-h-full mb-4 md:max-h-screen">
+      <div className="relative flex items-center justify-center w-full h-full bg-gradient-to-bl from-hyper to-blush">
         <img
           src={featureImage?.imageUrl}
           alt={featureImage?.description ?? title}
+          style={{ maxHeight: '65vh', width: '87.5em' }}
           className="object-fill"
         />
       </div>
 
       {featureImage?.description && (
-        <div className="w-8/12 pl-2 text-sm italic">
-          {featureImage?.description}
-        </div>
+        <Contained>
+          <ArticleContained>
+            <div className="pt-2 italic ext-sm">
+              {featureImage?.description}
+            </div>
+          </ArticleContained>
+        </Contained>
       )}
     </div>
   );

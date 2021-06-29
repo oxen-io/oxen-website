6b4a438a1f8df9d9a59523d323c4e24fe0c80a55
Lucas Phang <lucasphang98@gmail.com>
2021-06-29 17:50:18 +1000

    Remove surrounding image padding

--- components/article/sections/ArticleSectionFeatureImage.tsx
+++ components/article/sections/ArticleSectionFeatureImage.tsx
@@ -8,16 +8,18 @@ interface Props {
 export function ArticleSectionFeatureImage({ featureImage, title }: Props) {
   return (
     <div className="w-full pb-4 desktop:pb-0">
-      <div className="relative w-full mb-4 aspect-w-14 aspect-h-8">
+      <div className="relative w-full max-h-full mb-4 md:max-h-screen">
         <img
           src={featureImage?.imageUrl}
           alt={featureImage?.description ?? title}
-          className="object-cover rounded"
+          className="object-fill"
         />
       </div>
 
       {featureImage?.description && (
-        <div className="w-8/12 text-sm italic">{featureImage?.description}</div>
+        <div className="w-8/12 pl-2 text-sm italic">
+          {featureImage?.description}
+        </div>
       )}
     </div>
   );

478374481d5ec320c99bad9814c93e9158f1864c
William Grant <willmgrant@gmail.com>
2021-09-20 11:54:00 +1000

    removed custom webpack code and removed unneeded babel deps

--- next.config.js
+++ next.config.js
@@ -4,20 +4,6 @@ const withFonts = require('next-fonts');
 const withSvgr = require('next-svgr');
 
 const nextConfig = {
-  webpack(config) {
-    const rules = [{}];
-
-    return {
-      ...config,
-      module: {
-        ...config.module,
-        rules: [...config.module.rules, ...rules],
-      },
-      node: {
-        fs: 'empty',
-      },
-    };
-  },
   env: {
     CONTENTFUL_SPACE_ID: process.env.CONTENTFUL_SPACE_ID,
     CONTENTFUL_ACCESS_TOKEN: process.env.CONTENTFUL_ACCESS_TOKEN,

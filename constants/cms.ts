5a332c3aae510a1f12ae955b88219b99dc26e48c
William Grant <willmgrant@gmail.com>
2022-02-22 09:17:06 +1100

    upgraded next and sharp, changed regeneration time to every 10 minutes

--- constants/cms.ts
+++ constants/cms.ts
@@ -35,9 +35,9 @@ const CMS = {
   },
   BLOG_RESULTS_PER_PAGE: 13,
   BLOG_RESULTS_PER_PAGE_TAGGED: 12,
-  // Next.js will attempt to re-generate the page when a request comes in
-  // hourly for the main site and every 30 seconds for staging
-  CONTENT_REVALIDATE_RATE: isLive() ? 3600 : 30,
+  // Next.js will try and re-build the page when a request comes in
+  // every 10 minutues for production and every 30 seconds for staging
+  CONTENT_REVALIDATE_RATE: isLive() ? 600 : 30,
 };
 
 export default CMS;

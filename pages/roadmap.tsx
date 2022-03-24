49e7ed832272c1df2d917d1e7f12fe6d49e1e2a6
William Grant <willmgrant@gmail.com>
2022-03-24 16:07:00 +1100

    removed roadmap title, removed year from navbar

--- pages/roadmap.tsx
+++ pages/roadmap.tsx
@@ -13,27 +13,22 @@ function Roadmap() {
         metadata={METADATA.ROADMAP_PAGE}
       />
       <div className="w-full h-full">
-        <div className="flex flex-col px-6 pb-6 space-y-10">
-          <div>
-            <h2 className="mt-6 mb-3 text-3xl font-medium text-center tablet:text-4xl font-prompt text-primary">
-              Oxen in 2022
-            </h2>
-            <div
-              className={classNames(
-                'relative w-full mx-auto',
-                'desktop:max-w-3xl',
-              )}
-            >
-              <Image
-                src={`/img/roadmap.webp`}
-                alt="Oxen's Roadmap and Plans for the future."
-                width={1920}
-                height={3528}
-                layout="responsive"
-                quality={100}
-                priority={true}
-              />
-            </div>
+        <div className="flex flex-col px-6 py-6 space-y-10">
+          <div
+            className={classNames(
+              'relative w-full mx-auto',
+              'desktop:max-w-3xl',
+            )}
+          >
+            <Image
+              src={`/img/roadmap.webp`}
+              alt="Oxen's Roadmap and Plans for the future."
+              width={1920}
+              height={3528}
+              layout="responsive"
+              quality={100}
+              priority={true}
+            />
           </div>
         </div>
       </div>
